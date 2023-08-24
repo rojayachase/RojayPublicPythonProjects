8f05b9f37b9a9a842a47206484e07627c5bdde45
Rojay <54439866+rojayachase@users.noreply.github.com>
2023-08-24 09:29:59 -0400

    formatting currency

--- main.py
+++ main.py
@@ -3,19 +3,25 @@ user_name = input("Enter Your Name? ")
 
 ##print welcome message
 print(f"Hello, {user_name}, Welcome to your Interest Calculator!" "\n"
-      "We will determine the Total Interest paid overtime ")
+     "We will determine the Total Interest paid overtime ")
 
-total_debt = int(input("How much do you owe your car loan?"))
-interest_rate = float(input("What is your interest rate?"))
-loan_term = int(input("How many years left do you have on your loan term?"))
+##storing data 
+total_debt = int(input("How much do you owe your car loan Ex. 45354?  "))
+interest_rate = float(input("What is your interest rate Ex. 3.98?  "))
+loan_term = int(input("How many years left do you have on your loan term Ex. 7?  "))
+
+
 
 ## Principal loan amount x interest rate x loan term = interest
+interest = (float(total_debt * interest_rate/100 * loan_term))
+##rounding interest
+rounded_interest = round(interest, 2)
+##currency format
+formated_interest = "{:,}".format(rounded_interest)
 
-interest = float(total_debt * interest_rate/100 * loan_term)
 
-print(f"Hello, {user_name}, Your total interest over the course of {loan_term} years is ${interest}!" "\n"
+print(f"Hello, {user_name}, Your total interest over the course of {loan_term} years is ${formated_interest}!" "\n"
       "Keep going, you're doing great :)")
-
 
 
 
